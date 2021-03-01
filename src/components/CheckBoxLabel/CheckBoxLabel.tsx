50a917624a8219c192a1eb3ac20b6670f5ae2a0c
yt-ymmt <ftbae1@gmail.com>
2021-03-01 13:36:52 +0900

    fix: stylelint

--- src/components/CheckBoxLabel/CheckBoxLabel.tsx
+++ src/components/CheckBoxLabel/CheckBoxLabel.tsx
@@ -27,9 +27,9 @@ const Wrapper = styled.div`
 const Label = styled.label<{ themes: Theme }>`
   ${({ themes }) => {
     const { palette } = themes
+    // 複数行テキストに対応させるため flex-start にする
     return css`
       display: flex;
-      // 複数行テキストに対応させるため flex-start にする
       align-items: flex-start;
       color: ${palette.TEXT_BLACK};
       cursor: pointer;
@@ -45,11 +45,11 @@ const Label = styled.label<{ themes: Theme }>`
 const Txt = styled.span<{ themes: Theme }>`
   ${({ themes }) => {
     const { size } = themes
+    // checkbox と text の位置がずれるため、line-height 分を調整する疑似要素を作る
     return css`
       margin: 0 0 0 ${size.pxToRem(size.space.XXS)};
       font-size: ${size.pxToRem(size.font.TALL)};
 
-      // checkbox と text の位置がずれるため、line-height 分を調整する疑似要素を作る
       &::before {
         content: '';
         display: block;
